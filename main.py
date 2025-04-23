fd1ddc6310e724419833a8d21c972e3404209797
Turkeyt00th <rory.h.walker@gmail.com>
2025-04-23 15:24:46 +0100

    Created game GUI and added infinite while loop

--- main.py
+++ main.py
@@ -5,10 +5,23 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Game loop
+    running = True
+    while running:
+        # Event handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        # 3. Draw the game onto the screen
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
